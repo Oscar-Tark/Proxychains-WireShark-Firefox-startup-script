46e0a10426939701f462b5c50103feef03a89a26
Oscar-Tark <oscartark91@gmail.com>
2022-05-28 13:54:31 +0200

    Bug fixes and improvements to log file handling.

--- Proxychains.sh
+++ Proxychains.sh
@@ -6,7 +6,7 @@ function crtl_c(){
 	echo "Exiting.. Cleaining up Firefox and Wireshark"
 }
 
-datetime=`date +"%d_%m_%y_%H_%M"`
+datetime=`date +"%d.%m.%y::%H:%M"`
 $wireshark
 $proxychains
 $devname
@@ -14,15 +14,15 @@ $wiresharkdevice
 $logfile
 
 #Create log file
-$logfile="/var/log/proxychainswireshark${datetime}.capture"
-If file exists add an index
+logfile="/var/log/proxychainswireshark${datetime}.capture"
+#If file exists add an index
 for i in {0..4}
 do
-	if [ -f "$logfile" ]; then
-		continue
-	else
-		sudo touch $logfile.=i
+	if [ ! -f "$logfile" ]; then
+		sudo touch "${logfile}"
 		break
+	else
+		logfile="/var/log/proxychainswireshark${i}.${datetime}.capture"
 	fi
 done
 
@@ -31,19 +31,18 @@ if [ ! -f "$logfile" ]; then
         sudo touch "/var/log/proxychainswireshark${datetime}.capture"
 fi
 
-
-echo "Starting Wireshark and Firefox with proxychains"
+#Select wireshark device
 echo "Device Selection for wireshark:"
 sudo wireshark -D
 echo "Please insert the index or name of a device:"
-read $devname
+read devname
 
 #Set commands
-$wireshark="sudo wireshark -i ${devname} -k -w /var/log/proxychainswireshark${datetime}.capture"
-$proxychains="proxychains firefox -p -no-remote"
+wireshark="sudo wireshark -i ${devname} -k -w ${logfile}"
+proxychains="proxychains firefox -p -no-remote"
 
 #Start Wireshark and proxychains
-echo "Starting Wireshark (Log@/var/log/proxychainswireshark${datetime}.capture)"
+echo "Starting Wireshark (Log@${logfile})"
 $wireshark &
 echo "Starting Firefox with proxychains"
 $proxychains &
